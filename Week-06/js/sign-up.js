fb483f1f20a728872db0664ce15f8a9cb8bfae3b
Juan.Manuel <juanmlantermo@gmail.com>
2023-04-25 19:14:28 -0300

    BaSP-W06-2023:validation-sign up

--- Week-06/js/sign-up.js
+++ Week-06/js/sign-up.js
@@ -10,13 +10,14 @@ function hasInteger(string) {
         }
     }
 }
-// **************  NAME  ********************
+
 var firstName = document.getElementById('name');
 firstName.addEventListener('keyup', isName);
-
+var savedName;
 function isName(e) {
     var text = e.target.value;
     if (!hasInteger(text) && text.length > 3) {
+        savedName = text;
         firstName.onblur = function () {
             firstName.classList.remove('not-valid');
             firstName.classList.add('valid');
@@ -38,13 +39,14 @@ function isName(e) {
         }
     }
 }
-// **************  LASTNAME ********************
+
 var lastName = document.getElementById('last-name');
 lastName.addEventListener('keyup', isLastName);
-
+var savedLastName;
 function isLastName(e) {
     var text = e.target.value;
     if (!hasInteger(text) && text.length > 3) {
+        savedLastName= text;
         lastName.onblur = function () {
             lastName.classList.remove('not-valid');
             lastName.classList.add('valid');
@@ -66,15 +68,16 @@ function isLastName(e) {
         }
     }
 }
-// **************  EMAIL  ********************
+
 var email = document.getElementById('email');
 email.addEventListener('keyup', isEmail);
-
+var savedEmail;
 function isEmail(e) {
     var text = e.target.value;
     var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
     var result = emailExpression.test(text);
     if (result) {
+        savedEmail=text;
         email.onblur = function () {
             email.classList.remove('not-valid');
             email.classList.add('valid');
@@ -96,16 +99,19 @@ function isEmail(e) {
         }
     }
 }
+var savedDate;
+var dateOn = document.getElementById('birth');
+dateOn.setAttribute('min', '1950-01-01');
+dateOn.setAttribute('max', '2005-12-31');
+savedDate=dateOn;
 
-// **************  BIRTH  ********************  Con formato dd/mm/aaaa.
-
-// **************  DNI  ********************
 var dni = document.getElementById('dni');
 dni.addEventListener('keyup', isDNI);
-
+var savedDni ;
 function isDNI(e) {
     var text = e.target.value;
-    if (text.indexOf(' ') == -1 && text.toUpperCase() == text.toLowerCase() && text.length >= 7 && text.length <8) {
+    if (text.indexOf(' ') == -1 && text.toUpperCase() == text.toLowerCase() && text.length >= 7) {
+        savedDni=text;
         dni.onblur = function () {
             dni.classList.remove('not-valid');
             dni.classList.add('valid');
@@ -127,13 +133,14 @@ function isDNI(e) {
         }
     }
 }
-// **************  PHONE  ********************
+
 var phone = document.getElementById('phone');
 phone.addEventListener('keyup', isPhone);
-
+var savedPhone;
 function isPhone(e) {
     var text = e.target.value;
-    if (text.indexOf(' ') == -1 && text.toUpperCase() == text.toLowerCase() && text.length >= 10) {
+    if (text.indexOf(' ') == -1 && text.toUpperCase() == text.toLowerCase() && text.length >= 7) {
+        savedPhone = text;
         phone.onblur = function () {
             phone.classList.remove('not-valid');
             phone.classList.add('valid');
@@ -155,65 +162,176 @@ function isPhone(e) {
         }
     }
 }
-// **************  ADRESS ********************
+
 var address = document.getElementById('address');
 address.addEventListener('keyup', isAddress);
+var savedAddress;
 
 function isAddress(e) {
     var text = e.target.value;
-    if (text.indexOf(' ') > 3 && text.toUpperCase() != text.toLowerCase() && hasInteger(text) && text.length >= 5) {
-        return console.log(true);
+    if (text.indexOf(' ') >= 3 && text.lastIndexOf(' ') <= text.length - 3 && text.toUpperCase() != text.toLowerCase() && hasInteger(text) && text.length >= 5) {
+        savedAddress = text;
+        address.onblur = function () {
+            address.classList.remove('not-valid');
+            address.classList.add('valid');
+        }
     } else {
-        return console.log(false);
+        var errorAlert;
+        address.onblur = function () {
+            address.classList.remove('valid');
+            address.classList.add('not-valid');
+            errorAlert = document.createElement('span');
+            errorAlert.className = 'alert';
+            var textAlert = document.createTextNode('This input must have at least 5 letters, 1 number and a blank space.');
+            errorAlert.appendChild(textAlert);
+            address.insertAdjacentElement('afterend', errorAlert);
+        }
+        address.onfocus = function () {
+            address.classList.remove('valid', 'not-valid');
+            errorAlert.remove();
+        }
     }
 }
-// **************  CITY  ********************
+
 var city = document.getElementById('city');
 city.addEventListener('keyup', isCity);
+var savedCity;
 
 function isCity(e) {
     var text = e.target.value;
-    if (text.length > 3) {
-        return console.log(true);
+    if (text = text.toLowerCase() && text.length > 3) {
+        savedCity = text;
+        city.onblur = function () {
+            city.classList.remove('not-valid');
+            city.classList.add('valid');
+        }
     } else {
-        return console.log(false);
+        var errorAlert;
+        city.onblur = function () {
+            city.classList.remove('valid');
+            city.classList.add('not-valid');
+            errorAlert = document.createElement('span');
+            errorAlert.className = 'alert';
+            var textAlert = document.createTextNode('This input must have at least 3 digits without symbols.');
+            errorAlert.appendChild(textAlert);
+            city.insertAdjacentElement('afterend', errorAlert);
+        }
+        city.onfocus = function () {
+            city.classList.remove('valid', 'not-valid');
+            errorAlert.remove();
+        }
     }
 }
-// **************  ZIP CODE  ********************
+
 var zipCode = document.getElementById('zip-code');
 zipCode.addEventListener('keyup', isZipCode);
+var savedZipCode;
 
 function isZipCode(e) {
     var text = e.target.value;
-    if (text.indexOf(' ') == -1 && text.toUpperCase() == text.toLowerCase() && text.length >= 3 && text.length <= 5) {
-        return console.log(true);
+    if (text.indexOf(' ') == -1 && text.toUpperCase() == text.toLowerCase() && text.length >= 4 && text.length <= 5) {
+        savedZipCode = text;
+        zipCode.onblur = function () {
+            zipCode.classList.remove('not-valid');
+            zipCode.classList.add('valid');
+        }
     } else {
-        return console.log(false);
+        var errorAlert;
+        zipCode.onblur = function () {
+            zipCode.classList.remove('valid');
+            zipCode.classList.add('not-valid');
+            errorAlert = document.createElement('span');
+            errorAlert.className = 'alert';
+            var textAlert = document.createTextNode('This input must have between 4 and 5 numbers.');
+            errorAlert.appendChild(textAlert);
+            zipCode.insertAdjacentElement('afterend', errorAlert);
+        }
+        zipCode.onfocus = function () {
+            zipCode.classList.remove('valid', 'not-valid');
+            errorAlert.remove();
+        }
     }
 }
-// **************  PASS  ********************
+function haveNumber(string) {
+    var chars = [];
+    for (var i = 0; i < string.length; i++) {
+        chars.push(parseInt(string[i]));
+    }
+    for (var i = 0; i < chars.length; i++) {
+        if (!Number.isInteger(chars[i])) {
+        } else {
+            return true;
+        }
+    }
+}
 var password = document.getElementById('password');
 password.addEventListener('keyup', isPassword);
+var savedPassword;
 
-var savedPassword = '';
 function isPassword(e) {
     var textPassword = e.target.value;
-    if (textPassword.indexOf(' ') == -1 && textPassword.toUpperCase() != textPassword.toLowerCase() && hasInteger(textPassword) && textPassword.length >= 8) {
+    if (textPassword.indexOf(' ') == -1 && textPassword.toUpperCase() != textPassword.toLowerCase() && haveNumber(textPassword) && textPassword.length >= 8) {
         savedPassword = textPassword;
-        return console.log(true);
+        password.onblur = function () {
+            password.classList.remove('not-valid');
+            password.classList.add('valid');
+        }
     } else {
-        return console.log(false);
+        var errorAlert;
+        password.onblur = function () {
+            password.classList.remove('valid');
+            password.classList.add('not-valid');
+            errorAlert = document.createElement('span');
+            errorAlert.className = 'alert';
+            var textAlert = document.createTextNode('Incorrect password format');
+            errorAlert.appendChild(textAlert);
+            password.insertAdjacentElement('afterend', errorAlert);
+        }
+        password.onfocus = function () {
+            password.classList.remove('valid', 'not-valid');
+            errorAlert.remove();
+        }
     }
 }
-// **************  REPEAT PASS  ********************
+
 var repeatPassword = document.getElementById('repeat-password');
 repeatPassword.addEventListener('keyup', isTheSamePassword);
 
 function isTheSamePassword(e) {
     var textRepeatPassword = e.target.value;
     if (savedPassword == textRepeatPassword) {
-        return console.log(true);
+        repeatPassword.onblur = function () {
+            repeatPassword.classList.remove('not-valid');
+            repeatPassword.classList.add('valid');
+        }
     } else {
-        return console.log(false);
+        var errorAlert;
+        repeatPassword.onblur = function () {
+            repeatPassword.classList.remove('valid');
+            repeatPassword.classList.add('not-valid');
+            errorAlert = document.createElement('span');
+            errorAlert.className = 'alert';
+            var textAlert = document.createTextNode('This input must be exactly as the password input.');
+            errorAlert.appendChild(textAlert);
+            repeatPassword.insertAdjacentElement('afterend', errorAlert);
+        }
+        repeatPassword.onfocus = function () {
+            repeatPassword.classList.remove('valid', 'not-valid');
+            errorAlert.remove();
+        }
     }
 }
+function validateSubmit() {
+    if (email.classList.contains('not-valid') || password.classList.contains('not-valid') || firstName.classList.contains('not-valid') ||
+        lastName.classList.contains('not-valid') || dni.classList.contains('not-valid') || dateOn.classList.contains('not-valid') ||
+        phone.classList.contains('not-valid') || zipCode.classList.contains('not-valid') || address.classList.contains('not-valid') ||
+        city.classList.contains('not-valid') || repeatPassword.classList.contains('not-valid')) {
+        alert('There are some inputs with incorrect information.');
+    } else {
+        alert('Your name is: ' + savedName + '\nYour lastname is: ' + savedLastName + '\nYour mail is: ' + savedEmail + '.\nYour date is: '
+            + savedDate + '\nYour dni is: ' + savedDni + '\nYour phone is: ' + savedPhone + '\nYour address is: ' + savedAddress
+            + '\nYour city is: ' + savedCity + '\nYour zip code is: ' + savedZipCode + '\nYour password is: ' + savedPassword);
+    }
+}
+var registerBtn = document.getElementById('register');
+registerBtn.addEventListener('click', validateSubmit);
